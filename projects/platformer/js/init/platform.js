766a428afd483cbca2e3fd6eae4227b0ae2b725a
sellers11948 <sellers11948@students.apsb.org>
2021-10-05 16:11:38 +0000

    add platforms and collectibles to game

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -44,6 +44,8 @@
         createPlatform(400,460);
         createPlatform(300,200,0.3);
         createPlatform(500,500,0.3,10);
+        createPlatform(0,100);
+        
     }
     platform.init = init;
 })(window);
